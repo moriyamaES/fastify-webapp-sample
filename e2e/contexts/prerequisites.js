857c4feaa2400557a320bff79d42135994124d37
kazuhiro <moriyama.kazuhiro@earthsys-lab.co.jp>
2025-02-16 10:57:48 +0900

    2025/2/16書籍読み終了

--- e2e/contexts/prerequisites.js
+++ e2e/contexts/prerequisites.js
@@ -1,6 +1,11 @@
 const{ utils } = inject()
 
 module.exports = {
+  /**
+   * テスト用の商品データを作成する
+   * @param {string} name 商品名。未指定の場合は「牛ハラミ弁当-テスト-YYYYMMDDHHmmss」がセットされる
+   * @returns 商品名
+   */
   haveItem(name) {
     const I = actor({});  
     if (!name) name = `牛ハラミ弁当-テスト-${utils.now.format("YYYYMMDDHHmmss")}`;
